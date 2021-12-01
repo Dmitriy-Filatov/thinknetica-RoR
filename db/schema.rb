c58364d675d42322989ea6fc870ec591788344d6
Dmitriy-Filatov <zakon@uma.su>
2021-12-01 20:37:26 +0300

    migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_092303) do
+ActiveRecord::Schema.define(version: 2021_12_01_172444) do
+
+  create_table "questions", force: :cascade do |t|
+    t.text "body"
+    t.integer "test_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
