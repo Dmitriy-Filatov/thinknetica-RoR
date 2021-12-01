bfece1c0b3edf7b8fee2506b7d371c00d674a7f5
Dmitriy-Filatov <zakon@uma.su>
2021-12-01 21:16:44 +0300

    migrate models user category

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_180104) do
+ActiveRecord::Schema.define(version: 2021_12_01_181447) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.integer "question"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -32,6 +38,12 @@ ActiveRecord::Schema.define(version: 2021_12_01_180104) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
