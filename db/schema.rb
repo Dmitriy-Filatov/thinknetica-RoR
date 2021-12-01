63ce7999f37f1b8258e782f920d467629b735c6d
Dmitriy-Filatov <zakon@uma.su>
2021-12-01 21:04:27 +0300

    migrate model answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_172444) do
+ActiveRecord::Schema.define(version: 2021_12_01_180104) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.integer "question"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.text "body"
