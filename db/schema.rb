9921fe3d4f87c1c0d84b1b602b7df4ec213f5a77
Dmitriy-Filatov <zakon@uma.su>
2021-12-06 18:26:17 +0300

    g model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_144014) do
+ActiveRecord::Schema.define(version: 2021_12_06_152350) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.boolean "correct"
-    t.integer "questions_id"
+    t.integer "tests_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["questions_id"], name: "index_answers_on_questions_id"
+    t.index ["tests_id"], name: "index_answers_on_tests_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2021_12_06_144014) do
     t.integer "category"
   end
 
-  add_foreign_key "answers", "questions", column: "questions_id"
+  create_table "users", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "answers", "tests", column: "tests_id"
   add_foreign_key "questions", "tests", column: "tests_id"
 end
