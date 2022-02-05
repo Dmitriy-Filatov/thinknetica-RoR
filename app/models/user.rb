0048ff3d5fb591bc932020a370bfbe6f5499ee4e
Dmitriy-Filatov <zakon@uma.su>
2022-02-05 19:25:09 +0300

    add def returns_a_list_of_all_tests(difficulty_level_value)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  def returns_a_list_of_all_tests(difficulty_level_value)
+    #that the User passes or has ever passed
+    #at this level of difficulty
+    #
+  end
 end
