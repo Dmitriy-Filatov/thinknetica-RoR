38c9d82eaf6c44e47967dcdf42ef6659e76cc571
Dmitriy Filatov <zakon@uma.su>
2022-02-06 01:41:08 +0300

    chore(model Test): added some word into the comment

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
   belongs_to :category
 
   def self.returns_array_of_titles_of_all_tests(category_name)
-    #sorted_in_descending_order
+    #sorted_in_descending_fucking_order
   end
 end
