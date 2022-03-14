85060232b504ca99233fd61132dbb659e829f235
Dmitriy Filatov <zakon@uma.su>
2022-03-14 21:42:22 +0300

    feat(test.rb): add two has_many associations and new class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,11 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :tests_users
+  has_many :users, through: :tests_users
 
   def self.returns_array_of_titles_of_all_tests(category_name)
-    #sorted_in_descending_fucking_order
+    joins(:category)
+      .where(category: { title: category_name })
+      .order(title: :desc).pluck(:title)
   end
 end
