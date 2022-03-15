905312d343fe20126001e3c3467953253bde667d
Dmitriy Filatov <zakon@uma.su>
2022-03-15 20:13:47 +0300

    chore(20220313134028_create_tests_users.rb): add two belongs_to associations

--- db/migrate/20220313134028_create_tests_users.rb
+++ db/migrate/20220313134028_create_tests_users.rb
@@ -3,6 +3,8 @@ class CreateTestsUsers < ActiveRecord::Migration[6.1]
     create_table :tests_users do |t|
       t.integer :test_id, index: true, null: false
       t.integer :user_id, index: true, null: false
+      t.belongs_to :test, foreign_key: true
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
