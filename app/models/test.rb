53e07df35ac1d34ff5a49445523d8a2cf1262d50
Dmitriy Filatov <zakon@uma.su>
2022-03-21 22:02:44 +0300

    feat(test.rb): add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,7 @@
 class Test < ApplicationRecord
+  belongs_to :author, class_name: 'User'
   belongs_to :category
+  has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
 
