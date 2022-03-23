e0246fd7e05d00bd8eb9ca1f24778d5c1acbaad3
Dmitriy Filatov <zakon@uma.su>
2022-03-23 23:28:57 +0300

    feat(schema.rb): add_column_to_tests.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_13_134028) do
+ActiveRecord::Schema.define(version: 2022_03_23_202140) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2022_03_13_134028) do
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
