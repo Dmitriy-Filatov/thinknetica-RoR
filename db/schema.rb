23b959ad9e336b4f9b3327b3326654c05fd3155f
Dmitriy Filatov <zakon@uma.su>
2022-03-27 02:54:42 +0300

    feat(db/schema.rb):add   add_foreign_key "tests", "users", column: "author_id"

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,8 @@ ActiveRecord::Schema.define(version: 2022_03_23_202140) do
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "author_id"
+    t.integer "author_id", null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -63,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_03_23_202140) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
   add_foreign_key "tests_users", "tests"
   add_foreign_key "tests_users", "users"
 end
