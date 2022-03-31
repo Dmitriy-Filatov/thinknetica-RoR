456adc37ebce03b613adc3de96502ad98f3bf028
Dmitriy Filatov <zakon@uma.su>
2022-03-31 23:29:35 +0300

    feat(test.rb): add validates

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,11 +5,14 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
-  scope :easy, -> { where(level: 0..1) }
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal: 0 }
 
   scope :easy, -> { where(level: 0..1) }
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :level, ->(level) { where(level: level) }
   scope :get_categories_by_title, ->(title) { joins(:category).where(category: { title: title }).order(title: :desc).pluck(:title) }
 
   def self.returns_array_of_titles_of_all_tests_by_category_title(title)
