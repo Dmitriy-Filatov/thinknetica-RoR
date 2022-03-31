ad1a05d8a2e00fe0281d677926ddca41a8670cb0
Dmitriy Filatov <zakon@uma.su>
2022-03-31 23:19:03 +0300

    feat(answer.rb): add validates

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,11 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   scope :correct_answer, -> { where(correct: true) }
+
+  validates :body, presence: true
+  validate :limits_the_range_of_answers, on: %i[create update]
+
+  def limits_the_range_of_answers
+    errors.add(:question, "too many answers") if question.answers.count >= 4
+  end
 end
