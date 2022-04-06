a56a5b90fbacc34e64a4022f9d4ceeccd9a520f2
Dmitriy Filatov <zakon@uma.su>
2022-04-07 00:47:46 +0300

    fix(tests.rb): move pluck(:title) from scope to class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -13,9 +13,9 @@ class Test < ApplicationRecord
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
   scope :level, ->(level) { where(level: level) }
-  scope :get_categories_by_title, ->(title) { joins(:category).where(category: { title: title }).order(title: :desc).pluck(:title) }
+  scope :get_categories_by_title, ->(title) { joins(:category).where(category: { title: title }).order(title: :desc) }
 
   def self.returns_array_of_titles_of_all_tests_by_category_title(title)
-    get_categories_by_title(title)
+    get_categories_by_title(title).pluck(:title)
   end
 end
