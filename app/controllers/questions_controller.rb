0162753c60176d92b63574bc46d03e48db5fdee0
Dmitriy Filatov <zakon@uma.su>
2022-09-18 12:05:35 +0300

    feat(app/controllers): questions controller (no callbacks)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,42 @@
 class QuestionsController < ApplicationController
+
+  def show; end
+
+  def index
+    @questions = @test.questions
+
+    render plain: @questions.inspect
+    # render inline: '<%= @questions.inspect %>'
+  end
+
+  def create
+    question = Question.create(question_params)
+
+    render plain: question.inspect
+  end
+
+  def destroy
+    @question.destroy
+
+    render plain: 'The question has been deleted.'
+    # render html: '<h1>The question has been deleted.</h1>'.html_safe
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found.'
+  end
 end
