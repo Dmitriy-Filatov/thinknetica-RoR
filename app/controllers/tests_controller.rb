29d0682af41153c089c5bf595e71db32c5ca6d82
Dmitriy Filatov <zakon@uma.su>
2022-09-21 17:41:15 +0300

    chore: some changes

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,31 @@
 class TestsController < ApplicationController
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    #byebug
+
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level)
+  end
 end
