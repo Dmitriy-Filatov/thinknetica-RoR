2f39a90884bbe3d7e730f07c66e05f1a02c539d5
Dmitriy-Filatov <zakon@uma.su>
2022-10-05 14:37:42 +0300

    chore(app/controllers/tests_controller.rb): rename atribute

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -45,7 +45,7 @@ class TestsController < ApplicationController
   end
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
+    params.require(:test).permit(:title, :level, :category_id, :user_id)
   end
 
   def rescue_with_test_not_found
