bc9e1018a31e70e7beeb9d53b2c66314eeb38248
Dmitriy-Filatov <zakon@uma.su>
2022-10-11 13:52:56 +0300

    feat(app/models/user.rb): add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :created_tests, class_name: 'Test'
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :test_passages
+  has_many :tests, through: :test_passages
+  has_many :created_tests, class_name: 'Test', foreign_key: :user_id
 
   validates :email, presence: true, length: { within: 5..40 }
 
