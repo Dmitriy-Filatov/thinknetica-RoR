fbcf21b0afed631c488400c33be1769c094da3a8
Dmitriy-Filatov <zakon@uma.su>
2022-10-11 16:53:51 +0300

    feat(db/schema.rb): add default value

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2022_10_11_110616) do
     t.integer "user_id", null: false
     t.integer "test_id", null: false
     t.integer "current_question_id", null: false
-    t.integer "correct_question"
+    t.integer "correct_question", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
