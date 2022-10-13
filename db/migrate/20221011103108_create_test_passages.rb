b2d3d1102b048fefc95fcc05b0ac05809e00a765
Dmitriy-Filatov <zakon@uma.su>
2022-10-13 13:12:42 +0300

    feat(app): add before_validation_set_first_question method

--- db/migrate/20221011103108_create_test_passages.rb
+++ db/migrate/20221011103108_create_test_passages.rb
@@ -4,7 +4,7 @@ class CreateTestPassages < ActiveRecord::Migration[6.1]
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
       t.references :question, null: false, foreign_key: true
-      t.integer :correct_question_id
+      t.integer :correct_questions, default: 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2022_10_11_110616) do
     t.integer "user_id", null: false
     t.integer "test_id", null: false
     t.integer "question_id", null: false
-    t.integer "correct_question_id"
+    t.integer "correct_questions"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_test_passages_on_question_id"
