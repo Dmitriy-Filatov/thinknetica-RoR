26eeb4df637e1e61c1ef44a929b8883c28ea4eca
Dmitriy-Filatov <zakon@uma.su>
2022-10-13 15:33:49 +0300

    feat(app/models/test_passage.rb): add methods

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,10 +4,27 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', foreign_key: :question_id, optional: true
 
   before_validation :before_validation_set_first_question, on: :create
-end
 
-private
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
 
-def before_validation_set_first_question
-  self.current_question = test.questions.first if test.present?
+    self.currenr_question = next_question
+    save
+  end
+
+  private
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers.count == correct_answers.where(id: answer_ids).count
+  end
+
+  def correct_answers
+    qurrent_question.answers.correct
+  end
 end
