f98f0523a5fa5a8f5eb0ec265a469db420e7310f
Dmitriy-Filatov <zakon@uma.su>
2022-10-15 15:08:51 +0300

    feat(db/schema.rb): add options

--- db/schema.rb
+++ db/schema.rb
@@ -38,11 +38,11 @@ ActiveRecord::Schema.define(version: 2022_10_11_110616) do
   create_table "test_passages", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "test_id", null: false
-    t.integer "question_id", null: false
-    t.integer "correct_questions"
+    t.integer "current_question_id"
+    t.integer "correct_questions", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id"], name: "index_test_passages_on_question_id"
+    t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
   end
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 2022_10_11_110616) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "test_passages", "questions"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
   add_foreign_key "test_passages", "tests"
   add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
