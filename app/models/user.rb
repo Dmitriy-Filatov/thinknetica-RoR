7952eae57489da888ebd5350543193a62ce1f158
Dmitriy-Filatov <zakon@uma.su>
2022-10-15 15:01:49 +0300

    feat(app/models/user.rb): add options

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   has_many :test_passages
   has_many :tests, through: :test_passages
-  has_many :created_tests, class_name: 'Test', foreign_key: :user_id
+  has_many :created_tests, class_name: 'Test', inverse_of: :author, foreign_key: :author_id, dependent: :destroy
 
   validates :email, presence: true, length: { within: 5..40 }
 
