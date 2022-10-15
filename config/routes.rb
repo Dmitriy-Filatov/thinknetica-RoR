bb96ef3325a64a4f716ded9f057660fadf2e04bb
Dmitriy-Filatov <zakon@uma.su>
2022-10-15 15:08:03 +0300

    feat(config/routes.rb): add route

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,10 @@ Rails.application.routes.draw do
       resources :answers, shallow: true, except: :index
     end
 
-    member do
-      post :start
-    end
+    post :start, on: :member
   end
 
   resources :test_passages, only: %i[show update] do
-    get :result#, on: :member
+    get :result, on: :member
   end
 end
