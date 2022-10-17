140e030d2c116149e9130369790d6e9558acf4c3
Dmitriy-Filatov <zakon@uma.su>
2022-10-17 17:32:39 +0300

    feat(app/models/test.rb): add default scope, methods options and edit level scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,22 +1,25 @@
 class Test < ApplicationRecord
-  has_many :test_passages
-  has_many :users, through: :test_passages
-  has_many :questions
-  belongs_to :category#, optional: true
-  belongs_to :author, class_name: 'User', inverse_of: :created_tests, foreign_key: :author_id#, optional: true
+  default_scope { order(created_at: :desc) }
 
-  validates :level, numericality: { only_integer: true, greater_than: 0 }
+  belongs_to :category, optional: true
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  has_many :test_passages, dependent: :destroy
+  has_many :users, through: :test_passages
+  has_many :questions, dependent: :destroy
+
   validates :title, presence: true, uniqueness: {
     scope: %i[level category],
     case_sensitive: false,
     message: 'already exists'
   }
+  validates :level, numericality: { only_integer: true, greater_than: 0 }
 
-  scope :easy, -> { level(1) }
+  scope :level, ->(level) { where(level: level) }
+  scope :easy, -> { where(level: 1) }
   scope :elementary, -> { where(level: 2) }
   scope :advanced, -> { where(level: 2) }
   scope :hard, -> { where(level: 4..Float::INFINITY) }
-  scope :level, ->(level) { where(level: level) }
   scope :get_categories_by_title, ->(title) { joins(:category).where(category: { title: title }) }
 
   def self.titles_of_all_tests_by_category_title(title)
