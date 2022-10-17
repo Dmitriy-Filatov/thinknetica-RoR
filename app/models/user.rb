66c3a4fc58cd5364ba3a94d34a5b0d8ff8692aa2
Dmitriy-Filatov <zakon@uma.su>
2022-10-17 17:52:15 +0300

    feat(app/models/user.rb): add option, delete option, delete id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
 
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
-  has_many :created_tests, class_name: 'Test', inverse_of: :author, foreign_key: :author_id, dependent: :destroy
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
   validates :email, presence: true, length: { within: 5..40 }
 
   def test_passage(test)
-    test_passages.order(id: :desc).find_by(test_id: test.id)
+    test_passages.order(id: :desc).find_by(test_id: test)
   end
 
   def returns_a_list_of_all_tests(level_value)
