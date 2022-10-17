f61203cee50c9c8fb37052efc1c7b96ae9902b8b
Dmitriy-Filatov <zakon@uma.su>
2022-10-17 16:22:04 +0300

    feat(app/models/answer.rb): add validates

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,11 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   scope :correct, -> { where(correct: true) }
+
+  validates :body, presence: true
+  validates :validate_max_limit_answers, on: %i[create update]
+
+  def validate_max_limit_answers
+    errors.add(:question, 'limit is exceeded') unless question.answers.count < 4
+  end
 end
