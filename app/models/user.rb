0511d901d46e8c77ade6571cbd6c4c7cf26cdc88
Dmitriy-Filatov <zakon@uma.su>
2022-10-20 17:11:16 +0300

    chore(app/models/user.rb): rename method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
-  has_many :created_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
+  has_many :authored_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
   validates :email, presence: true, length: { within: 5..40 }
 
