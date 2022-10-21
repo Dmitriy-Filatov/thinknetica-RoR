e3473ad5ad42cac2369054876fe01cef0ec636cc
Dmitriy-Filatov <zakon@uma.su>
2022-10-21 12:01:19 +0300

    feat(db/seeds.rb): add password argument

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create!([{ name: 'Иван', email: 'ivan@durak.su' }])
+users = User.create!([{ name: 'Иван', email: 'ivan@durak.su', password: 'mypassword' }])
 
 categories = Category.create!([
   { title: 'Публичные мероприятия' },
