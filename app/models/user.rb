de6621310c8c53a57e3229fb6abe7bb2ef3b0c79
Dmitriy-Filatov <zakon@uma.su>
2022-10-21 15:07:17 +0300

    feat(app/model/user.rb): add Rails has_sequre_password method for including Rails BCrypt framework

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
+  has_sequre_password
+
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test)
   end
