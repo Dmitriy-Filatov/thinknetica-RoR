fc12e402043cfb01f0fa632e046e1b32700be918
Dmitriy-Filatov <zakon@uma.su>
2022-10-21 17:33:34 +0300

    fix: grammar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
-  include Auth
 
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
-  has_sequre_password
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test)
