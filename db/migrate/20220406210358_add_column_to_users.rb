e9e1f4bad74332f3160a04a3a470b728b95eafed
Dmitriy-Filatov <zakon@uma.su>
2022-10-21 12:59:40 +0300

    chore(db/migrate/20220406210358_add_column_to_users.rb): remove default argument

--- db/migrate/20220406210358_add_column_to_users.rb
+++ db/migrate/20220406210358_add_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column(:users, :email, :string, limit: 40, default: 'email', null: false)
+    add_column(:users, :email, :string, limit: 40, null: false)
   end
 end
