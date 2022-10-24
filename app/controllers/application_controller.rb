9868ed906c15e66c78f2ba39e0220d7be66711a0
Dmitriy-Filatov <zakon@uma.su>
2022-10-24 15:50:58 +0300

    feat(app/controllers/application_controller.rb): add callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  helper_method :current_user,
-                :logged_in?
+  helper_method :current_user, :logged_in?
+
+  before_action :authenticate_user!
 
   private
 
