17757ade5c652e817f1faf6e854c4217d99342b7
Dmitriy-Filatov <zakon@uma.su>
2022-10-24 15:53:10 +0300

    feat(app/controllers/sessions_controller.rb): change redirect_to

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  
   def new; end
 
   def create
@@ -6,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to tests_path
+      redirect_to cookies.delete(:redirect_user) || tests_path
     else
       flash.now[:alert] = 'Are you a Guru? Verify your Email and Password please.'
       render :new
