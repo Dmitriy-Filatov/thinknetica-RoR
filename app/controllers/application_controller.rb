23a7f918775b9343067c4c528ca55d82a197c46f
Dmitriy-Filatov <zakon@uma.su>
2022-10-24 12:55:30 +0300

    feat(app/controllers/application_controller.rb): add class helpers and alert message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,15 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user,
+                :logged_in?
 
   private
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path
+      redirect_to login_path, alert:
+        'Are you a Guru? Verify your Email and Password please.'
     end
   end
 
