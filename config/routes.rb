e442afd517a565952f7f79e00c9defbc9c0ead3c
Dmitriy-Filatov <zakon@uma.su>
2022-10-25 17:57:25 +0300

    chore(config/routes.rb): change route

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
   get 'users/new'
-  
+
   root 'tests#index'
 
   get :signup, to: 'users#new'
   get :login, to: 'sessions#new'
-  get :signout, to: 'sessions#destroy'
+  delete :signout, to: 'sessions#destroy'
 
   resources :users, only: :create
   resources :sessions, only: :create
