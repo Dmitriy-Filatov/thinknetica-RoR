11d92af25217b596a455fd71599a99e8615d113e
Dmitriy-Filatov <zakon@uma.su>
2022-10-26 12:23:17 +0300

    feat(app/models/user.rb): include default devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
 
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
-
-  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test)
