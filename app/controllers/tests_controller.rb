2edb47f6ab681924c8ee14da16de0e24cad4046c
Dmitriy-Filatov <zakon@uma.su>
2022-10-26 21:57:49 +0300

    feat(app/controllers/tests_controller.rb): modify tests controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,44 +1,11 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_test, only: %i[start]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
     @tests = Test.all
-  end
-
-  def show; end
-
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = Test.new(test_params)
-    if @test.save
-      redirect_to @test, notice: 'Test created.'
-    else
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test, notice: 'Test updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    if @test.destroy
-      redirect_to tests_path, notice: 'Test destroyed.'
-    else
-      redirect_to tests_path
-    end
   end
 
   def start
@@ -50,10 +17,6 @@ class TestsController < ApplicationController
 
   def set_test
     @test = Test.find(params[:id])
-  end
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 
   def rescue_with_test_not_found
