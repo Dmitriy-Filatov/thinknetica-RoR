3582494ad5ef3e19f92c530bd92531877cd24a88
Dmitriy-Filatov <zakon@uma.su>
2022-10-27 12:38:50 +0300

    chore(app/models/answer.rb): edit contents

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,10 @@
 class Answer < ApplicationRecord
   belongs_to(:question)
 
-  scope(:correct, -> { where(correct: true) })
-
   validates(:body, presence: true)
   validate(:validate_max_limit_answers, { on: %i[create update] })
+
+  scope(:correct, -> { where(correct: true) })
 
   def validate_max_limit_answers
     errors.add(:question, 'limit is exceeded') if question.answers.count > 4
