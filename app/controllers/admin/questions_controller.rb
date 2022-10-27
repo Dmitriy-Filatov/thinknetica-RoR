0485787b321f2e04eb25069ce8fe29a8b6375faa
Dmitriy-Filatov <zakon@uma.su>
2022-10-27 13:33:44 +0300

    feat(app/controllers/admin): move files

---
+++ app/controllers/admin/questions_controller.rb
@@ -0,0 +1,61 @@
+class QuestionsController < ApplicationController
+
+  before_action :find_test, only: %i[new create]
+  before_action :find_question, only: %i[show edit update destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  # 'R' (Read) of CRUD
+
+  def show; end
+
+  # 'C' (Create) of CRUD
+  def new
+    @question = @test.questions.new
+  end
+
+  def create
+    @question = @test.questions.new(question_params)
+
+    if @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  # 'U' (Update) of CRUD
+  def edit; end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # 'D' (Delete) of CRUD
+  def destroy
+    @question.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body).to_h.merge(test_id: params[:test_id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found.'
+  end
+end
