a83d00c143af160d9fdd12effaeb217bdd010b7d
Dmitriy-Filatov <zakon@uma.su>
2022-10-28 20:47:06 +0300

    feat(app): edit test_passages

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,15 +1,9 @@
 module TestPassagesHelper
-  def result_color_and_percent_message(result_percent)
-    if @test_passage.success?
-      "<h2 style='color: green'>You answered #{result_percent}% questions.
-      The Test passed successfully!</h2>".html_safe
-    else
-      "<h2 style='color: red'>You answered #{result_percent}% questions.
-      The Test failed!</h2>".html_safe
-    end
+  def result_message(test_passage)
+    test_passage.success? ? t('.success') : t('.failure')
   end
 
-  def question_number(test_passage)
-    "The Question #{test_passage.current_question_number} of #{test_passage.questions_count}."
+  def green_red(test_passage)
+    test_passage.success? ? 'greed' : 'red'
   end
 end
