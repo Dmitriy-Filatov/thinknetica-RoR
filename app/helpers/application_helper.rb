cda665fb60555b8e6c70cc9545f4204e1220cd96
Dmitriy-Filatov <zakon@uma.su>
2022-10-31 19:03:37 +0300

    feat(app/helpers/application_helper.rb): add and change methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
+  FLASH_MESSAGE_STYLE = { notice: 'success', alert: 'danger' }
+
   def current_year
-    "Current year: #{Date.current.year}"
+    Date.current.year
   end
 
   def github_url(author, repo)
     link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
+
+  def greeting_in_header
+    current_user.first_name || current_user.email
   end
 end
