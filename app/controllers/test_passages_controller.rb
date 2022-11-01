1c53caaebac03ce03363cad302791d2479056fd2
Dmitriy-Filatov <zakon@uma.su>
2022-11-01 16:38:47 +0300

    feat(app/services/gist_question_service.rb): add methods for gist

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,10 +1,8 @@
 class TestPassagesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_test_passage, only: %i[show update result]
+  before_action :set_test_passage, only: %i[show update result gist]
 
   def show; end
-
-  def result; end
 
   def update
     @test_passage.accept!(params[:answer_ids])
@@ -17,9 +15,34 @@ class TestPassagesController < ApplicationController
     end
   end
 
+  def result; end
+
+  def gist
+    result = GistQuestionService.new(@test_passage.current_question).call
+
+    if result.success?
+      gist = gist_create(result.html_url)
+
+      flash[:notice] = t('.success', url: view_context.link_to(t('links.gist'), gist.gist_url, target: '_blank'))
+    else
+      flash[:alert] = t('.failure')
+    end
+
+    redirect_to @test_passage
+  end
+
   private
+
+  def gist_create(url)
+    Gist.create!(
+        question: @test_passage.current_question,
+        gist_url: url,
+        user: current_user
+        )
+  end
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
   end
 end
+ 
