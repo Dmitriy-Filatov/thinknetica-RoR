73a0b40804eba080fe01c71879075d3808053191
Dmitriy-Filatov <zakon@uma.su>
2022-11-02 21:01:33 +0300

    feat(app/models/question.rb): add relations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
-  belongs_to :test
+  belongs_to :test, touch: true
   has_many :answers, dependent: :destroy
+  has_many :gists, dependent: :destroy
 
   validates :body, presence: true
 end
