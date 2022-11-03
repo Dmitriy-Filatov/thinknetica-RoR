01676f39f9606bd46c9b0dbc055060bc81f26f03
Dmitriy-Filatov <zakon@uma.su>
2022-11-03 12:43:58 +0300

    fix(app/controllers/test_passages_controller.rb): remove  def gist_create(url) and add current_user.gists.create

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -21,7 +21,10 @@ class TestPassagesController < ApplicationController
     result = GistQuestionService.new(@test_passage.current_question).call
 
     if result.success?
-      gist = gist_create(result.html_url)
+      current_user.gists.create(
+        question: @test_passage.current_question,
+        url: result.html_url
+      )
 
       flash[:notice] = t('.success', url: view_context.link_to(t('links.gist'), gist.gist_url, target: '_blank'))
     else
@@ -33,16 +36,7 @@ class TestPassagesController < ApplicationController
 
   private
 
-  def gist_create(url)
-    Gist.create!(
-        question: @test_passage.current_question,
-        gist_url: url,
-        user: current_user
-        )
-  end
-
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
   end
 end
-
