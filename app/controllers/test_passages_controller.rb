3261d017dd4bd761d9db317156c43f67e4f27839
Dmitriy-Filatov <zakon@uma.su>
2022-11-21 17:42:03 +0300

    feat(app/controllers/test_passages_controller.rb): add method

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TestPassagesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_test_passage, only: %i[show update result gist]
@@ -21,10 +23,7 @@ class TestPassagesController < ApplicationController
     result = GistQuestionService.new(@test_passage.current_question).call
 
     if result.success?
-      current_user.gists.create(
-        question: @test_passage.current_question,
-        url: result.html_url
-      )
+      gist = gist_create(result.html_url)
 
       flash[:notice] = t('.success', url: view_context.link_to(t('links.gist'), gist.gist_url, target: '_blank'))
     else
@@ -35,6 +34,14 @@ class TestPassagesController < ApplicationController
   end
 
   private
+
+  def gist_create(url)
+    Gist.create!(
+      question: @test_passage.current_question,
+      gist_url: url,
+      user: current_user
+    )
+  end
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
