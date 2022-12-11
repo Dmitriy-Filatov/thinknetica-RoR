156cf464666362c113f6ce1b0cbd1006b4e67bef
Dmitriy-Filatov <zakon@uma.su>
2022-12-11 11:42:36 +0300

    chore(app/controllers/feedbacks_controller.rb): add resoult_of_feedback_success_failure_pre_creation_check method

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -3,10 +3,19 @@
 class FeedbacksController < ApplicationController
   respond_to :html
 
-  def new
-  end
+  def new; end
 
   def create
+    resoult_of_feedback_success_failure_pre_creation_check
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:body, :email, :first_name, :last_name)
+  end
+
+  def resoult_of_feedback_success_failure_pre_creation_check
     email = feedback_params[:email]
 
     if feedback_params[:body].present? && email.present? && URI::MailTo::EMAIL_REGEXP.match?(email)
@@ -16,11 +25,5 @@ class FeedbacksController < ApplicationController
       flash.now[:alert] = t('.failure')
       render :new
     end
-  end
-
-  private
-
-  def feedback_params
-    params.require(:feedback).permit(:body, :email, :first_name, :last_name)
   end
 end
